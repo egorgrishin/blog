5dc3df9a775127b40417e6bd3289073f85de69f0
Egor Grishin <egor-grishin@bk.ru>
2022-06-29 00:59:41 +0500

    back base

--- database/migrations/2022_06_28_194322_create_tag_post_table.php
+++ database/migrations/2022_06_28_194322_create_tag_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTagPostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tag_post', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('tag_id')->constrained('tags');
+            $table->foreignId('post_id')->constrained('posts');
         });
     }
 
@@ -30,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tag_post');
     }
 }
