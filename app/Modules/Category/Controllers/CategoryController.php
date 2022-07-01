5343e5ed638fba1fc516afc2b4610ce7c20f26cf
Egor Grishin <egor-grishin@bk.ru>
2022-07-01 05:55:07 +0500

    controllers

---
+++ app/Modules/Category/Controllers/CategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Category\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    //
+}
---
+++ app/Modules/Post/Controllers/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Post\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    //
+}
