ddb5d0b52d57dfdab54db74457ae9779efe21713
Egor Grishin <egor-grishin@bk.ru>
2022-07-01 06:10:41 +0500

    models

--- app/Modules/User/Models/User.php
+++ app/Modules/User/Models/User.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace App\Models;
+namespace App\Modules\User\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
