ec0cd86ca58f8080bcf6426ec245d7561fdcb63c
Egor Grishin <egor-grishin@bk.ru>
2022-07-03 17:16:38 +0500

    migrations

--- database/migrations/2022_07_01_010651_create_posts_table.php
+++ database/migrations/2022_07_01_010651_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->text('text');
             $table->timestamps();
         });
     }
--- database/migrations/2022_07_01_010707_create_comments_table.php
+++ database/migrations/2022_07_01_010707_create_comments_table.php
@@ -15,6 +15,8 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->text('text');
             $table->timestamps();
         });
     }
