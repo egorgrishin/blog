4c19fe9a3f0a8ebc939afd736e4994faeb213a2c
Egor Grishin <egor-grishin@bk.ru>
2022-07-07 06:07:46 +0500

    router middleware

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,8 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from "./routes";
+import { loginCheckApi } from '../api/auth'
+import { useAuthStore } from '../stores/auth'
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach(async (to) => {
+  const authStore = useAuthStore()
+  if (authStore.isLogged === null)
+    authStore.isLogged = (await loginCheckApi()).status
+
+  if (to.path !== '/login' && to.path !== '/register')
+    return true
+
+  return authStore.isLogged ? '/' : true
+})
+
 export default router
